32016f1492f20d0e5b1bacdff45a015e727036e4
General Kenobi <GeneralKenobi2485@gmail.com>
2024-10-05 13:03:15 -0400

    Images Wooooooo

--- Backend/get_landsat_api_data.py
+++ Backend/get_landsat_api_data.py
@@ -16,16 +16,18 @@ APIKey = jsonDict['data']
 print(f"API KEY: {APIKey}\n")
 
 
-def datasetSearch(APIKey, lowerLeft: list, upperRight: list, startTime: str, endTime: str):
+def datasetSearch(APIKey, lowerLeft: list, upperRight: list, startTime: str, endTime: str, maxCloudCover: int, minCloudCover: int):
     # Inputs:
     # lowerLeft: [lat, long]
     # upperRight: [lat, long]
     # startTime: "yyyy-mm-dd"
     # endTime: "yyyy-mm-dd"
-    datasetURL = url + "dataset-search"
+    datasetURL = url + f"scene-search"
     datasetData = {
-        "apikey": APIKey,
-        "datasetName": "Global Land Survey",
+    "maxResults": 1,
+    "datasetName": "gls_all",
+    "sceneFilter": {
+        "ingestFilter": None,
         "spatialFilter": {
             "filterType": "mbr",
             "lowerLeft": {
@@ -37,16 +39,27 @@ def datasetSearch(APIKey, lowerLeft: list, upperRight: list, startTime: str, end
                     "longitude": upperRight[1]
             }
         },
-        "temporalFilter": {
-            "start": startTime,
-            "end": endTime
-        }
-    }
-    response = requests.get(datasetURL, json=datasetData)
+        "metadataFilter": None,
+        "cloudCoverFilter": {
+            "max": minCloudCover,
+            "min": maxCloudCover,
+            "includeUnknown": False
+        },
+        "acquisitionFilter": None
+    },
+    "bulkListName": "my_bulk_list",
+    "metadataType": "summary",
+    "orderListName": "my_order_list",
+    "startingNumber": 1,
+    "compareListName": "my_comparison_list",
+    "excludeListName": "my_exclusion_list"
+}
+    response = requests.get(datasetURL, json=datasetData, headers={'X-Auth-Token':APIKey})
     print(f"Dataset Error Code: {response.status_code}")
-    print(f"Dataset JSON: {response.json()}\n")
+    print(f"Dataset JSON: {response.json()}\n\n\n")
+    print(f"Data: {json.loads(f'{json.dumps(response.json())}')['data']}\n\n\n")
 
-datasetSearch(APIKey=APIKey, lowerLeft=[44.60847, -99.69639], upperRight=[44.60847, -99.69639], startTime="2012-01-01", endTime="2012-12-01")
+datasetSearch(APIKey=APIKey, lowerLeft=[44.60847, -99.69639], upperRight=[44.60847, -99.69639], startTime="2012-01-01", endTime="2012-12-01", minCloudCover=0, maxCloudCover=100)
 
 logoutURL = url + "logout"
 response = requests.get(logoutURL, json={})
