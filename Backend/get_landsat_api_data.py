1cdf7f34a8f97fbbe0fde44fb7a51e8213c93c3a
General Kenobi <GeneralKenobi2485@gmail.com>
2024-10-05 13:52:26 -0400

    Function Returns Images

--- Backend/get_landsat_api_data.py
+++ Backend/get_landsat_api_data.py
@@ -16,15 +16,18 @@ APIKey = jsonDict['data']
 print(f"API KEY: {APIKey}\n")
 
 
-def datasetSearch(APIKey, lowerLeft: list, upperRight: list, startTime: str, endTime: str, maxCloudCover: int, minCloudCover: int):
+def datasetSearch(APIKey, lowerLeft: list, upperRight: list, startTime: str, endTime: str, maxCloudCover: int, minCloudCover: int, maxResults: int):
     # Inputs:
     # lowerLeft: [lat, long]
     # upperRight: [lat, long]
     # startTime: "yyyy-mm-dd"
     # endTime: "yyyy-mm-dd"
+    # maxCloudCover: int
+    # minCloudCover: int
+    # maxResults: int
     datasetURL = url + f"scene-search"
     datasetData = {
-    "maxResults": 1,
+    "maxResults": maxResults,
     "datasetName": "gls_all",
     "sceneFilter": {
         "ingestFilter": None,
@@ -45,7 +48,10 @@ def datasetSearch(APIKey, lowerLeft: list, upperRight: list, startTime: str, end
             "min": maxCloudCover,
             "includeUnknown": False
         },
-        "acquisitionFilter": None
+        "acquisitionFilter": {
+            "end": endTime,
+            "start": startTime
+        }
     },
     "bulkListName": "my_bulk_list",
     "metadataType": "summary",
@@ -58,10 +64,19 @@ def datasetSearch(APIKey, lowerLeft: list, upperRight: list, startTime: str, end
     print(f"Dataset Error Code: {response.status_code}")
     print(f"Dataset JSON: {response.json()}\n\n\n")
     print(f"Data: {json.loads(f'{json.dumps(response.json())}')['data']}\n\n\n")
+    for n in range(0, maxResults):
+        try:
+            image = json.loads(f'{json.dumps(response.json())}')['data']['results'][n]['browse'][0]['browsePath']
+            images = str(image)
+        except IndexError:
+            print("haha funny to make code work")
+        
+        return images
+        
 
-datasetSearch(APIKey=APIKey, lowerLeft=[44.60847, -99.69639], upperRight=[44.60847, -99.69639], startTime="2012-01-01", endTime="2012-12-01", minCloudCover=0, maxCloudCover=100)
+datasetSearch(APIKey=APIKey, lowerLeft=[-12.3, -12.3], upperRight=[12.3, 12.3], startTime="2005-01-16", endTime="2018-09-13", minCloudCover=0, maxCloudCover=100, maxResults=10)
 
 logoutURL = url + "logout"
-response = requests.get(logoutURL, json={})
+response = requests.get(logoutURL, json={}, headers={'X-Auth-Token':APIKey})
 print(f"Logout Error Code: {response.status_code}")
 print(f"Logout JSON: {response.json()}")
