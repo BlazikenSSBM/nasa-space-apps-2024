0b3c6a6b370c6de5d089148ce961263412245143
hoadi <94995420+Hoadi605@users.noreply.github.com>
2024-10-06 18:43:50 -0400

    Final landsat api data code

--- Backend/get_landsat_api_data.py
+++ Backend/get_landsat_api_data.py
@@ -53,23 +53,22 @@ def datasetSearch(coordinate: list, startTime: str, endTime: str, maxCloudCover:
     "excludeListName": "my_exclusion_list"
 }
     response = requests.get(datasetURL, json=datasetData, headers={'X-Auth-Token':APIKey})
-    print(f"Dataset Error Code: {response.status_code}")
-    print(f"Dataset JSON: {response.json()}\n\n\n")
-    print(f"Data: {json.loads(f'{json.dumps(response.json())}')['data']}\n\n\n")
+    #print(f"Dataset Error Code: {response.status_code}")
+    #print(f"Dataset JSON: {response.json()}\n\n\n")
+    #print(f"Data: {json.loads(f'{json.dumps(response.json())}')['data']}\n\n\n")
     images = []
     index = 0
-    try:
-        while index < maxResults:
-            image = str(json.loads(f'{json.dumps(response.json())}')['data']['results'][index]['browse'][0]['browsePath'])
-            images.insert(-1, image)
-            index += 1
-    except:
-        return images
+
+    while index < len(json.loads(f'{json.dumps(response.json())}')['data']['results'][index]['browse'][0]['browsePath']):
+        image = str(json.loads(f'{json.dumps(response.json())}')['data']['results'][index]['browse'][0]['browsePath'])
+        images.insert(-1, image)
+        index += 1
+
     return images
         
 
 
-print(f"{datasetSearch(coordinate=[42.3043, -83.0660], startTime='2024-10-01', endTime='2024-10-06', maxCloudCover=100, minCloudCover=0, maxResults=1)}")
+print(f"{datasetSearch(coordinate=[42.3043, -83.0660], startTime='2023-01-01', endTime='2024-10-06', maxCloudCover=100, minCloudCover=0, maxResults=10000)}")
 
 # datasetSearch(coordinate=[lat, long], startTime='YYYY-MM-DD', endTime='YYYY-MM-DD', minCloudCover=int, maxCloudCover=int, maxResults=int)
 
